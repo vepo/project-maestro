c44c77eb0a34bed374ddd32f15a6e3fc9c3a9b77
Victor Osorio <victor.osorio@openet.com>
2024-08-17 13:24:00 -0300

    Implementing Config for CreateTopicDialog

--- maestro-kafka-manager/src/main/java/io/vepo/maestro/kafka/manager/dialogs/CreateTopicDialog.java
+++ maestro-kafka-manager/src/main/java/io/vepo/maestro/kafka/manager/dialogs/CreateTopicDialog.java
@@ -3,6 +3,10 @@ package io.vepo.maestro.kafka.manager.dialogs;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dialog.Dialog;
@@ -13,8 +17,10 @@ import com.vaadin.flow.component.textfield.IntegerField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.data.value.ValueChangeMode;
+import com.vaadin.flow.server.Platform;
 
 public class CreateTopicDialog extends Dialog {
+    private static final Logger logger = LoggerFactory.getLogger(CreateTopicDialog.class);
 
     public CreateTopicDialog() {
         super();
@@ -33,6 +39,11 @@ public class CreateTopicDialog extends Dialog {
     private class Configuration {
         private String key;
         private String value;
+
+        Configuration() {
+            key = "";
+            value = "";
+        }
 
         public String getKey() {
             return key;
@@ -53,6 +64,10 @@ public class CreateTopicDialog extends Dialog {
         @Override
         public String toString() {
             return String.format("Configuration [key=%s, value=%s]", key, value);
+        }
+
+        public boolean isBlank() {
+            return key == null || value == null || key.isBlank() || value.isBlank();
         }
     }
 
@@ -81,21 +96,36 @@ public class CreateTopicDialog extends Dialog {
         editor.setBuffered(true);
         var items = new ArrayList<>(List.of(new Configuration()));
         grid.setItems(items);
+        Runnable checkItems = () -> {
+            logger.info("Items: {}", items);
+            if (items.size() == 0 || !items.get(items.size() - 1).isBlank()) {
+                items.add(new Configuration());
+                UI.getCurrent().access(() -> grid.setItems(items));
+            }
+        };
         grid.addComponentColumn(c -> {
+            logger.info("Creating key component for {}", c);
             var txtKey = new TextField();
-            txtKey.setValueChangeMode(ValueChangeMode.EAGER);
+            txtKey.setValue(c.getKey());
+            txtKey.setWidthFull();
+            txtKey.setValueChangeMode(ValueChangeMode.ON_BLUR);
             txtKey.addValueChangeListener(e -> {
-                System.out.println("Value changed: " + e.getValue());
-                System.out.println("Items        : " + items);
-                System.out.println("Item         : " + c);
-                System.out.println("Items BINDER : " + binder.getBean());
-//                System.out.println("Items GRID   : " + grid.getDataProvider().fetch(new ArrayList<>()));
+                c.setKey(e.getValue());
+                checkItems.run();
             });
             binder.bind(txtKey, Configuration::getKey, Configuration::setKey);
             return txtKey;
         }).setHeader("Key");
         grid.addComponentColumn(c -> {
+            logger.info("Creating value component for {}", c);
             var txtValue = new TextField();
+            txtValue.setValue(c.getValue());
+            txtValue.setWidthFull();
+            txtValue.setValueChangeMode(ValueChangeMode.ON_BLUR);
+            txtValue.addValueChangeListener(e -> {
+                c.setValue(e.getValue());
+                checkItems.run();
+            });
             binder.bind(txtValue, Configuration::getValue, Configuration::setValue);
             return txtValue;
         }).setHeader("Value");
