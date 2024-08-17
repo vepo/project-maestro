813538526326383465be7b128aa07e60e89fa5f5
Victor Osorio <victor.osorio@openet.com>
2024-08-17 15:56:14 -0300

    CreateTopicDialog: Select by key

--- maestro-kafka-manager/src/main/java/io/vepo/maestro/kafka/manager/dialogs/CreateTopicDialog.java
+++ maestro-kafka-manager/src/main/java/io/vepo/maestro/kafka/manager/dialogs/CreateTopicDialog.java
@@ -1,12 +1,16 @@
 package io.vepo.maestro.kafka.manager.dialogs;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
 
+import org.apache.kafka.common.config.TopicConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.dialog.Dialog;
@@ -17,10 +21,8 @@ import com.vaadin.flow.component.textfield.IntegerField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.data.value.ValueChangeMode;
-import com.vaadin.flow.server.Platform;
 
 public class CreateTopicDialog extends Dialog {
-    private static final Logger logger = LoggerFactory.getLogger(CreateTopicDialog.class);
 
     public CreateTopicDialog() {
         super();
@@ -71,6 +73,32 @@ public class CreateTopicDialog extends Dialog {
         }
     }
 
+    private static final List<String> TOPIC_PROPERTIES = Stream.of(TopicConfig.CLEANUP_POLICY_CONFIG,
+                                                                   TopicConfig.COMPRESSION_TYPE_CONFIG,
+                                                                   TopicConfig.DELETE_RETENTION_MS_CONFIG,
+                                                                   TopicConfig.FILE_DELETE_DELAY_MS_CONFIG,
+                                                                   TopicConfig.FLUSH_MESSAGES_INTERVAL_CONFIG,
+                                                                   TopicConfig.FLUSH_MS_CONFIG,
+                                                                   TopicConfig.INDEX_INTERVAL_BYTES_CONFIG,
+                                                                   TopicConfig.MAX_COMPACTION_LAG_MS_CONFIG,
+                                                                   TopicConfig.MAX_MESSAGE_BYTES_CONFIG,
+                                                                   TopicConfig.MESSAGE_DOWNCONVERSION_ENABLE_CONFIG,
+                                                                   TopicConfig.MESSAGE_FORMAT_VERSION_CONFIG,
+                                                                   TopicConfig.MESSAGE_TIMESTAMP_DIFFERENCE_MAX_MS_CONFIG,
+                                                                   TopicConfig.MESSAGE_TIMESTAMP_TYPE_CONFIG,
+                                                                   TopicConfig.MIN_CLEANABLE_DIRTY_RATIO_CONFIG,
+                                                                   TopicConfig.MIN_COMPACTION_LAG_MS_CONFIG,
+                                                                   TopicConfig.MIN_IN_SYNC_REPLICAS_CONFIG,
+                                                                   TopicConfig.PREALLOCATE_CONFIG,
+                                                                   TopicConfig.RETENTION_BYTES_CONFIG,
+                                                                   TopicConfig.RETENTION_MS_CONFIG,
+                                                                   TopicConfig.SEGMENT_BYTES_CONFIG,
+                                                                   TopicConfig.SEGMENT_INDEX_BYTES_CONFIG,
+                                                                   TopicConfig.SEGMENT_JITTER_MS_CONFIG,
+                                                                   TopicConfig.SEGMENT_MS_CONFIG,
+                                                                   TopicConfig.UNCLEAN_LEADER_ELECTION_ENABLE_CONFIG)
+                                                               .sorted().toList();
+
     private FormLayout createDialogLayout() {
 
         var txtName = new TextField();
@@ -85,7 +113,6 @@ public class CreateTopicDialog extends Dialog {
         var dialogLayout = new FormLayout();
         var item = dialogLayout.addFormItem(txtName, "Name");
         dialogLayout.setColspan(item, 2);
-        // dialogLayout.setColspan(txtName, 2);
         dialogLayout.addFormItem(nmbPartitions, "Partitions");
         dialogLayout.addFormItem(nmbReplicationFactor, "Replication Factor");
 
@@ -95,16 +122,14 @@ public class CreateTopicDialog extends Dialog {
         editor.setBinder(binder);
         editor.setBuffered(true);
         var items = new ArrayList<>(List.of(new Configuration()));
-        grid.setItems(items);
+        var view = grid.setItems(items);
         Runnable checkItems = () -> {
-            logger.info("Items: {}", items);
-            if (items.size() == 0 || !items.get(items.size() - 1).isBlank()) {
-                items.add(new Configuration());
-                UI.getCurrent().access(() -> grid.setItems(items));
+            if (view.getItemCount() == 0 || !view.getItem(view.getItemCount() - 1).isBlank()) {
+                view.addItem(new Configuration());
             }
         };
         grid.addComponentColumn(c -> {
-            logger.info("Creating key component for {}", c);
+            AtomicInteger lastIndex = new AtomicInteger(-1);
             var txtKey = new TextField();
             txtKey.setValue(c.getKey());
             txtKey.setWidthFull();
@@ -113,11 +138,32 @@ public class CreateTopicDialog extends Dialog {
                 c.setKey(e.getValue());
                 checkItems.run();
             });
+            txtKey.addKeyDownListener(Key.ARROW_UP, e -> {
+                var currValue = lastIndex.updateAndGet(v -> {
+                    if (v == 0) {
+                        return TOPIC_PROPERTIES.size() - 1;
+                    } else {
+                        return v - 1;
+                    }
+                });
+                txtKey.setValue(TOPIC_PROPERTIES.get(currValue));
+            });
+            txtKey.addKeyDownListener(Key.ARROW_DOWN, e -> {
+                var currValue = lastIndex.updateAndGet(v -> {
+                    if (v == TOPIC_PROPERTIES.size() - 1) {
+                        return 0;
+                    } else {
+                        return v + 1;
+                    }
+
+                });
+                txtKey.setValue(TOPIC_PROPERTIES.get(currValue));
+            });
             binder.bind(txtKey, Configuration::getKey, Configuration::setKey);
             return txtKey;
-        }).setHeader("Key");
+        }).setHeader("Key")
+            .setFlexGrow(1);
         grid.addComponentColumn(c -> {
-            logger.info("Creating value component for {}", c);
             var txtValue = new TextField();
             txtValue.setValue(c.getValue());
             txtValue.setWidthFull();
@@ -128,12 +174,15 @@ public class CreateTopicDialog extends Dialog {
             });
             binder.bind(txtValue, Configuration::getValue, Configuration::setValue);
             return txtValue;
-        }).setHeader("Value");
+        }).setHeader("Value").setFlexGrow(1);
         grid.addComponentColumn(c -> new HorizontalLayout(new Button("Delete",
                                                                      e -> {
-                                                                         items.remove(c);
-                                                                         grid.setItems(items);
+                                                                         view.removeItem(c);
+                                                                         if (view.getItemCount() == 0) {
+                                                                             view.addItem(new Configuration());
+                                                                         }
                                                                      })))
+            .setFlexGrow(0)
             .setHeader("Actions");
         dialogLayout.setColspan(dialogLayout.addFormItem(grid, "Configurations"), 2);
 
