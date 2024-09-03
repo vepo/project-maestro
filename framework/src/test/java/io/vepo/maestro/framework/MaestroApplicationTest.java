be22abe90adae99cf76666f8dfed5405538e0812
Victor Osório <victor.osorio@amdocs.com>
2024-09-03 16:41:24 -0300

    Log

--- framework/src/test/java/io/vepo/maestro/framework/MaestroApplicationTest.java
+++ framework/src/test/java/io/vepo/maestro/framework/MaestroApplicationTest.java
@@ -1,21 +1,21 @@
 package io.vepo.maestro.framework;
 
 import org.junit.jupiter.api.Test;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.kafka.KafkaContainer;
+// import org.testcontainers.junit.jupiter.Container;
+// import org.testcontainers.junit.jupiter.Testcontainers;
+// import org.testcontainers.kafka.KafkaContainer;
 
-@Testcontainers
+// @Testcontainers
 class MaestroApplicationTest {
 
-    @Container
-    public KafkaContainer kafka = new KafkaContainer("apache/kafka-native:3.8.0");
+    // @Container
+    // public KafkaContainer kafka = new KafkaContainer("apache/kafka-native:3.8.0");
 
     
     @Test
     void testStart() {
         MaestroApplication maestroApplication = new MaestroApplication();
         maestroApplication.start();
-        System.out.println(kafka.getBootstrapServers());
+        // System.out.println(kafka.getBootstrapServers());
     }
 }
