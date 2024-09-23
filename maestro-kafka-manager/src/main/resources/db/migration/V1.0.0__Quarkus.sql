b6fa3456b05f922bdbf6a9d4d0f6b0d596649a8a
Victor Osorio <victor.osorio@openet.com>
2024-09-23 00:01:28 -0300

    Docker Build

--- maestro-kafka-manager/src/main/resources/db/migration/V1.0.0__Quarkus.sql
+++ maestro-kafka-manager/src/main/resources/db/migration/V1.0.0__Quarkus.sql
@@ -1,8 +1,9 @@
 CREATE TABLE cluster
 (
-  id   INT,
-  name VARCHAR(64),
-  bootstrapServers VARCHAR(256),
+  id   INT PRIMARY KEY,
+  name VARCHAR(64) NOT NULL,
+  bootstrapServers VARCHAR(256) NOT NULL,
+  CONSTRAINT cluster_name_unique UNIQUE (name)
 );
 
 CREATE SEQUENCE cluster_id_seq START 1;
